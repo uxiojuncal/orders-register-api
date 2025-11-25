f560685e5f54ea9adada4e78b09a6740cfb827a3
UxJuncal <100524757@alumnos.uc3m.es>
2025-11-25 16:51:40 +0100

    Code: Coded the search view

--- orders_register_api/database_api/models.py
+++ orders_register_api/database_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Order(models.Model):
     date = models.DateTimeField()
-    client_name = models.CharField(max_length=255)
+    customer_name = models.CharField(max_length=255)
     phone_number = models.IntegerField()
     receiver_name = models.CharField(max_length=255)
     product_name = models.CharField(max_length=255)
@@ -10,8 +10,8 @@ class Order(models.Model):
     comments = models.TextField(blank=True, null=True)
 
     class State(models.TextChoices):
-        PENDING = 'pending', 'Pending'
-        PROCESSING = 'processing', 'Processing'
-        DELIVERED = 'delivered', 'Delivered'
+        PENDING = 'pendiente', 'Pendiente'
+        PROCESSING = 'procesando', 'Procesando'
+        DELIVERED = 'entregado', 'Entregado'
 
     state = models.CharField(max_length=20, choices=State.choices, default=State.PENDING)
--- orders_register_api/database_api/views.py
+++ orders_register_api/database_api/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from .models import Order
 
 def create_order(request):
     pass
@@ -7,7 +8,21 @@ def update_order(request, pk):
     pass
 
 def search_orders(request):
-    pass
+    orders = Order.objects.none()
+
+    if any(request.GET.values()):
+        orders = Order.objects.all()
+
+        if customer_name := request.GET.get('customer_name'):
+            orders = orders.filter(customer_name__icontains=customer_name)
+        if status := request.GET.get('status'):
+            orders = orders.filter(status=status)
+        if id := request.GET.get('id'):
+            orders = orders.filter(id=id)
+        if date := request.GET.get('date'):
+            orders = orders.filter(date=date)
+        
+    return JsonResponse({'orders': list(orders.values())})
 
 def download_order_pdf(request, pk):
     pass
