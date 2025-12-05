80c0a2f6bbc158df715391350a3d40c9525798a4
UxJuncal <100524757@alumnos.uc3m.es>
2025-12-05 13:12:17 +0100

    Update urls.py
    added health endpoint to the urls

--- database_api/urls.py
+++ database_api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('orders/<int:pk>/', views.update_order, name='order-update'),
     path('orders/search/', views.search_orders, name='order-search'),          # GET => search with query params
     path('orders/<int:pk>/pdf/', views.generate_order_pdf, name='order-pdf'),  # GET => download PDF
-    path('orders/<int:pk>/signature/', views.upload_signature, name='order-signature')  # PATCH => upload signature
+    path('orders/<int:pk>/signature/', views.upload_signature, name='order-signature'),  # POST => upload signature
+    path('health/', views.health_check, name='health-check'),  # GET => health check
 ]
 
